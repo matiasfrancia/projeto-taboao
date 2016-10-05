2bc0f151fa747109ccf43ae51174e39d22ec4214
aipi <tesouraet@gmail.com>
2016-10-05 19:50:39 -0300

    nova arquitetura

---
+++ playScreen/playScreen.c
@@ -0,0 +1,117 @@
+// Os arquivos de cabeçalho
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+ 
+// Para utilizarmos o fprintf
+#include <stdio.h>
+ 
+// Atributos da tela
+const int LARGURA_TELA = 1024;
+const int ALTURA_TELA = 720;
+ 
+int main(void)
+{
+    ALLEGRO_DISPLAY *janela = NULL;
+    ALLEGRO_BITMAP *imagem = NULL;
+    ALLEGRO_BITMAP *pref1 = NULL;
+    ALLEGRO_BITMAP *pref2 = NULL;
+    ALLEGRO_BITMAP *pref3 = NULL;
+    ALLEGRO_BITMAP *fundo = NULL;
+    ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+ 
+    if (!al_init())
+    {
+        fprintf(stderr, "Falha ao inicializar a Allegro.\n");
+        return -1;
+    }
+ 
+    if (!al_init_image_addon())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_image.\n");
+        return -1;
+    }
+ 
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if (!janela)
+    {
+        fprintf(stderr, "Falha ao criar janela.\n");
+        return -1;
+    }
+
+    al_set_window_title(janela, "Projeto Taboão");
+
+ 
+    imagem = al_load_bitmap("Images/taboao.png");
+    if (!imagem)
+    {
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem0.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    pref1 = al_load_bitmap("Images/pref1.png");
+    if (!pref1){
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem1.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    pref2 = al_load_bitmap("Images/pref2.png");
+    if (!pref2){
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem2.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    pref3 = al_load_bitmap("Images/pref3.png");
+    if (!pref3){
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem3.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    fundo = al_load_bitmap("Images/fundoTaboao.png");
+    if (!pref3){
+        fprintf(stderr, "Falha ao carregar o arquivo de imagem4.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+    fila_eventos = al_create_event_queue();
+    if (!fila_eventos)
+    {
+        fprintf(stderr, "Falha ao criar fila de eventos.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
+ 
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+ 
+ 	al_draw_bitmap(fundo, 0, 0, 0);
+    al_draw_bitmap(imagem, 420, 150, 1);
+    al_draw_bitmap(pref1, 50, 300, 1);
+    al_draw_bitmap(pref2, 350, 300, 1);
+    al_draw_bitmap(pref3, 750, 300, 1);
+             
+ 
+    al_flip_display();
+ 
+    while (1){
+        ALLEGRO_EVENT evento;
+        ALLEGRO_TIMEOUT timeout;
+        al_init_timeout(&timeout, 0.5);
+ 
+        int tem_eventos = al_wait_for_event_until(fila_eventos, &evento, &timeout);
+ 
+        if (tem_eventos && evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+            break;
+        }
+ 
+        al_draw_bitmap(imagem, 420, 150, 1);
+        al_draw_bitmap(pref1, 50, 300, 1);
+        al_draw_bitmap(pref2, 350, 300, 1);
+        al_draw_bitmap(pref3, 750, 300, 1);
+        al_draw_bitmap(fundo, 0, 0, 0);
+        al_flip_display();
+    }
+
+    al_destroy_display(janela);
+    al_destroy_event_queue(fila_eventos);
+ 
+    return 0;
+}
