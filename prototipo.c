1f44142dd002352ed267bc76d589a4d6677cc05c
Henrique Shodi Maeta <japa1996@hotmail.com>
2016-11-27 23:27:46 -0200

    Funcao de aplicar evento aleatorio introduzida

--- prototipo.c
+++ prototipo.c
@@ -928,7 +928,8 @@ int playScreen(){
                         evento.mouse.y >= 344 && evento.mouse.y <= 361){
                     // botao de voltar quando evento ruim
                     
-                        cidade.dinheiro += ruim.dinheiro;                    
+                        cidade.dinheiro += ruim.dinheiro; 
+                        //aplica_evento_ruim(&cidade, ruim);                   
                         togglePopup = 3;
                         al_start_timer(contador);
                         toggleColor = 0;
@@ -938,7 +939,8 @@ int playScreen(){
                         evento.mouse.x >= 412 && evento.mouse.x <= 513 && 
                         evento.mouse.y >= 344 && evento.mouse.y <= 363){
                     // botao de aceitar quando evento bom
-                        cidade.dinheiro += bom.investimento;                    
+                        cidade.dinheiro -= bom.investimento;
+                        //aplica_evento_bom(&cidade, bom);                    
                         togglePopup = 3;
                         al_start_timer(contador);
                         toggleColor = 1;
